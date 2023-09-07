e0aeceb48920097acb3c6f29b860ad0b51615139
krishpatel2383 <krishpatel2383@gmail.com>
2023-09-07 16:57:37 +0530

    role based authorization

--- src/main/java/com/krish/EasyTrack/Config/SecurityConfig.java
+++ src/main/java/com/krish/EasyTrack/Config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.krish.EasyTrack.Config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -26,5 +30,20 @@ public class SecurityConfig {
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+//	@// @formatter:off
+ 
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.
+			authorizeHttpRequests((auth)->auth
+				.requestMatchers("/employees/**","/reports/**").hasRole("ADMIN")
+				.requestMatchers("/attendance/**").hasAnyRole("EMPLOYEE","ADMIN")
+				.anyRequest().authenticated())
+				.formLogin(withDefaults());
+		return http.build();
+	}
+	
+// @formatter:on
 
 }
